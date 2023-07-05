ca2605644bed312fb2222d54a650b6edf9262428
Edson Michaque <edson@michaque.com>
2023-07-05 09:01:26 +0200

    Add product operations

--- products.go
+++ products.go
@@ -2,8 +2,18 @@ package tdp
 
 type Products struct{}
 
-func (u Products) CreateProduct() {}
+func (u Products) CreateProduct(input ProductCreateInput) {}
 
-func (u Products) GetProductByID() {}
+func (u Products) GetProduct(id uint64) {}
 
-func (u Products) ListProducts() {}
+func (u Products) ListProducts(options ProductListOptions) {}
+
+func (u Products) UpdateProducts(id uint64, input ProductUpdateInput) {}
+
+type ProductUpdateInput struct {
+	Catalogues []uint64
+}
+
+type ProductCreateInput struct{}
+
+type ProductListOptions struct{}
