9fb823418e10268014b3e078cf9a949615be3432
Edson Michaque <edson@michaque.com>
2023-08-04 11:45:57 +0200

    Test success scenario of apps list operation

--- apps_test.go
+++ apps_test.go
@@ -10,6 +10,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestApps_Get(t *testing.T) {
@@ -39,12 +40,55 @@ func TestApps_Get(t *testing.T) {
 	assert.NoError(t, err)
 
 	want := []App{
-		{},{},{},{},{},
+		{
+			CreatedAt:    "2023-08-03 20:37:41",
+			Description:  "Foo",
+			ID:           5,
+			Name:         "Big App 2",
+			RedirectURLs: "",
+			UserID:       1,
+		},
+		{
+			CreatedAt:    "2023-08-03 20:37:07",
+			Description:  "",
+			ID:           4,
+			Name:         "Big App",
+			RedirectURLs: "",
+			UserID:       1,
+		},
+		{
+			CreatedAt:    "2023-08-03 19:45:32",
+			Description:  "",
+			ID:           3,
+			Name:         "xyz",
+			RedirectURLs: "",
+			UserID:       1,
+		},
+		{
+			CreatedAt:    "2023-08-03 00:22:48",
+			Description:  "",
+			ID:           2,
+			Name:         "app2",
+			RedirectURLs: "",
+			UserID:       1,
+		},
+		{
+			CreatedAt:    "2023-07-13 09:46:52",
+			Description:  "",
+			ID:           1,
+			Name:         "bgd",
+			RedirectURLs: "",
+			UserID:       1,
+		},
 	}
 
 	assertApps(t, want, resp.Data)
 }
 
 func assertApps(t *testing.T, want, got []App) {
-	assert.Equal(t, len(want), len(got), "wanted len %v but got len", len(want), len(got))
+	require.Equal(t, len(want), len(got), "wanted len %v but got len", len(want), len(got))
+
+	for k, v := range want {
+		assert.Equal(t, v, got[k])
+	}
 }
