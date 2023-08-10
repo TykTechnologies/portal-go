012fdf3efc5a82202a19a520e22af2b2b42384a9
Patricio Díaz <padiazg@gmail.com>
2023-08-09 23:58:50 -0400

    Fix error parsing date when getting AR details (#30)
    * Fix issue 29
    Error parsing date when getting an AR detail
    
    * Added test coverage

---
+++ custom-time.go
@@ -0,0 +1,29 @@
+package portal
+
+import (
+	"strings"
+	"time"
+)
+
+type CustomTime struct {
+	time.Time
+}
+
+func (ct *CustomTime) UnmarshalJSON(b []byte) error {
+	s := strings.Trim(string(b), "\"")
+	if s == "null" {
+		return nil
+	}
+
+	t, err := time.Parse("2006-01-02 15:04", s)
+	if err != nil {
+		return err
+	}
+
+	ct.Time = t
+	return nil
+}
+
+func (ct *CustomTime) MarshalJSON() []byte {
+	return []byte(ct.Time.Format("2006-01-02 15:04"))
+}
---
+++ custom-time_test.go
@@ -0,0 +1,72 @@
+package portal
+
+import (
+	"testing"
+	"time"
+)
+
+func TestCustomTime_UnmarshalJSON(t *testing.T) {
+	var (
+		t0, _ = time.Parse("2006-01-02 15:04", "2020-01-01 00:00")
+	)
+	tests := []struct {
+		name    string
+		Time    time.Time
+		b       []byte
+		wantErr bool
+	}{
+		{
+			name:    "Success",
+			Time:    t0,
+			b:       []byte("2020-01-01 00:00"),
+			wantErr: false,
+		},
+		{
+			name:    "Null-value",
+			Time:    t0,
+			b:       []byte("null"),
+			wantErr: false,
+		},
+		{
+			name:    "Fail",
+			Time:    t0,
+			b:       []byte("2020-01-01T00:00:00Z00:00"),
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ct := &CustomTime{
+				Time: tt.Time,
+			}
+			if err := ct.UnmarshalJSON(tt.b); (err != nil) != tt.wantErr {
+				t.Errorf("CustomTime.UnmarshalJSON() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestCustomTime_MarshalJSON(t *testing.T) {
+	tests := []struct {
+		name string
+		time time.Time
+		want []byte
+	}{
+		{
+			name: "Success",
+			time: time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC),
+			want: []byte("2020-01-01 00:00"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ct := &CustomTime{
+				Time: tt.time,
+			}
+			got := ct.MarshalJSON()
+			if string(got) != string(tt.want) {
+				t.Errorf("CustomTime.MarshalJSON() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
